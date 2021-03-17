e96fcd3e15428391faa189daad1ba0ffc55d9f86
atilla kalay <atilla.kalayy@gmail.com>
2021-03-17 23:29:37 +0300

    17.03.2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {BrowserAnimationsModule}from "@angular/platform-browser/animations"
 
 import{ToastrModule} from "ngx-toastr"
 import { from } from 'rxjs';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { from } from 'rxjs';
     CategoryComponent,
     NaviComponent,
     VatAddedPipe,
-    FilterPipePipe
+    FilterPipePipe,
+    CartSummaryComponent
   ],
   imports: [
     BrowserModule,
