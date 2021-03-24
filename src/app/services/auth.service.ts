d35915a1ec3ad16808181ed5454e0275a50979c8
atilla kalay <atilla.kalayy@gmail.com>
2021-03-24 22:08:51 +0300

    24.03.2021

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { LoginModel } from './../models/loginModel';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  apiUrl="https://localhost:44374/api/api/auth"
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+login(loginModel:LoginModel){
+return this.httpClient.post(this.apiUrl+"login",loginModel)
+}
+
+
+
 }
